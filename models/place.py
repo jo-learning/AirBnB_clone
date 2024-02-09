707ec8dbeb19439ba5f355fc9ddbb442f2aed950
Yohannes <yohannesguesh01@gmail.com>
2024-02-09 09:49:29 -0800

    all models

---
+++ models/place.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Defines the Place class."""
+from models.base_model import BaseModel
+
+
+class Place(BaseModel):
+    """place.
+
+    Attributes:
+        city_id (str): City id.
+        user_id (str): User id.
+        name (str): name of place
+        description (str): description of place
+        number_rooms (int): number of room
+        number_bathrooms (int): number of bathrooms
+        max_guest (int): maximum number of gust
+        price_by_night (int): price by night
+        latitude (float): latitude of the place.
+        longitude (float): longitude of the place.
+        amenity_ids (list): List of amenity ids
+    """
+
+    city_id = ""
+    user_id = ""
+    name = ""
+    description = ""
+    number_rooms = 0
+    number_bathrooms = 0
+    max_guest = 0
+    price_by_night = 0
+    latitude = 0.0
+    longitude = 0.0
+    amenity_ids = []
